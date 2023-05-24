35824935e8560a81a00f5649f4dba2776a5aee6b
imgrimist <134544021+imgrimist@users.noreply.github.com>
2023-05-24 17:28:18 -0400

    Create Config-File-Example.js

---
+++ Config-File-Example.js
@@ -0,0 +1,7 @@
+module.exports = {
+   attConfig: {
+     clientId: 'Put client ID here',
+     clientSecret: 'Put client secret here',
+     scope: [ 'ws.group', 'ws.group_members', 'ws.group_servers', 'ws.group_bans', 'ws.group_invites', 'group.info', 'group.join', 'group.leave', 'group.view', 'group.members', 'group.invite', 'server.view', 'server.console']
+  }
+} 
