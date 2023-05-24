6d4bcea0eb18d1ef76b02fceb9c77230e7cf6950
imgrimist <134544021+imgrimist@users.noreply.github.com>
2023-05-24 17:39:38 -0400

    Create PlayerMovedChunk-Event-Example.js

---
+++ PlayerMovedChunk-Event-Example.js
@@ -0,0 +1,26 @@
+const { Client: AttClient } = require('att-client');
+const { attConfig } = require('./bot-config');
+
+bot.on('connect', async (connection) => {
+    
+    // Subscribes to the PlayerMovedChunk event
+    connection.subscribe(`PlayerMovedChunk`, message => {
+        
+        // Declares the 'player, newChunk, oldChunk' constants
+        const { player, newChunk, oldChunk } = message.data;
+        
+        // Sees if player enters the Townhall
+        if (newChunk.startsWith(`Townhall Builing`)) {
+            connection.send(`player message ${player.id} "Welcome to Townhall, ${player.username}!" 4`);
+        }
+        
+        // Sees if player left Townhall
+        if (oldChunk.startsWith(`Townhall Building`)) {
+            connection.send(`player message ${player.id} "See you later, ${player.username}!" 4`);
+        }
+
+    })
+})
+
+// Start the bot
+bot.start()
