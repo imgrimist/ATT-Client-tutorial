442d8c611b2c23098057c4fb5022323786d6d1a7
imgrimist <134544021+imgrimist@users.noreply.github.com>
2023-05-24 17:21:01 -0400

    Create PlayerJoined-Event-Example.js

---
+++ PlayerJoined-Event-Example.js
@@ -0,0 +1,19 @@
+const { Client: AttClient } = require('att-client');
+const { attConfig } = require('./bot-config');
+
+bot.on('connect', async (connection) => {
+    
+        // Subscribes to the player joined event
+        connection.subscribe(`PlayerJoined`, message => {
+            
+            // Declares the 'user' constant
+            const { user } = message.data;
+            
+            // Sends the welcome message
+            connection.send(`player message * "${user.username} has joined ${connection.server.name}" 4`);
+
+        })
+})
+
+// Start the bot
+bot.start()
